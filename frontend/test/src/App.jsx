d32a8f0c8368fbf8862bd4ab15a566432ddfe896
Muhammad Talha <pnymeet@gmail.com>
2026-03-01 15:52:09 +0500

    removed css files

--- frontend/test/src/App.jsx
+++ frontend/test/src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <>
-    <h1>Hello From The Server</h1>
-  </>;
+  return (
+    <>
+      <h1>Hello From The Server</h1>
+    </>
+  );
 }
 
 export default App;
