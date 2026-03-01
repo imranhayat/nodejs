1fe868e655c0fb73b644d0ab318d4b37556d36a6
Muhammad Talha <pnymeet@gmail.com>
2026-03-01 15:47:23 +0500

    updated code

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
+
 const app = express();
-const PORT=5500;
+const PORT = 5500;
 
-app.use("/test", (req,res)=>{
-  res.json({message: "Server is running"})
-})
+// API route
+app.use("/test", (req, res) => {
+  res.json({ message: "Server is running" });
+});
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on PORT ${PORT}`)
-})
+// Serve Vite build folder
+app.use(express.static(path.join(__dirname, "dist")));
 
+// Handle React routing (SPA fallback)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "../frontend/test/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
